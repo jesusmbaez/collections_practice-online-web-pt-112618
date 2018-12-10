f47f8c308201832597c1327bf9e117cf89d5ecac
Manny Baez <jesusmbaez@gmail.com>
2018-12-10 21:09:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
+def sort_array_asc(array)
+  array.sort
+end 
+
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a
+  end 
+end 
+
+def sort_array_char_count(array)
+  array.sort_by! {|string| string.length}
+end 
 
